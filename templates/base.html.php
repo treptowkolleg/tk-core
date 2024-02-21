3d2449f7b52c9c2fbdbb42a91d69c5808121bac1
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 19:09:22 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -188,6 +188,20 @@ use TreptowKolleg\Api\Session;
             },
             svg: {
                 fontCache: 'global'
+            },
+            chtml: {
+                scale: 1,                      // global scaling factor for all expressions
+                minScale: .5,                  // smallest scaling factor to use
+                mtextInheritFont: false,       // true to make mtext elements use surrounding font
+                merrorInheritFont: true,       // true to make merror text use surrounding font
+                mathmlSpacing: false,          // true for MathML spacing rules, false for TeX rules
+                skipAttributes: {},            // RFDa and other attributes NOT to copy to the output
+                exFactor: .5,                  // default size of ex in em units
+                displayAlign: 'center',        // default for indentalign when set to 'auto'
+                displayIndent: '0',            // default for indentshift when set to 'auto'
+                matchFontHeight: true,         // true to match ex-height of surrounding font
+                fontURL: '[mathjax]/components/output/chtml/fonts/woff-v2',   // The URL where the fonts are found
+                adaptiveCSS: true              // true means only produce CSS that is used in the processed equations
             }
         };
     </script>
