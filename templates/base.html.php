c17578bd05bc89206fc9017644905821bbb16d81
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 18:41:12 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -146,6 +146,7 @@ use TreptowKolleg\Api\Session;
     </footer>
     <script src="/dist/app.js"></script>
     <script src="/assets/math.js"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
     </body>
 </html>
