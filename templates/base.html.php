8b3acee64c4897a97ec4c2caa4455a88a25c6ba5
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 18:53:04 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -160,8 +160,8 @@ use TreptowKolleg\Api\Session;
                 processRefs: true,         // process \ref{...} outside of math mode
                 digits: /^(?:[0-9]+(?:\{,\}[0-9]{3})*(?:\.[0-9]*)?|\.[0-9]+)/,
                 // pattern for recognizing numbers
-                tags: 'none',              // or 'ams' or 'all'
-                tagSide: 'right',          // side for \tag macros
+                tags: 'all',              // or 'ams' or 'all'
+                tagSide: 'left',          // side for \tag macros
                 tagIndent: '0.8em',        // amount to indent tags
                 useLabelIds: true,         // use label name rather than tag for ids
                 maxMacros: 10000,          // maximum number of macro substitutions per expression
