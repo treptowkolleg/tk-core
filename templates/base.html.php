eb1a51cb1ec1f97b40e35c633332f527d7fe4280
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 18:42:53 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -146,7 +146,9 @@ use TreptowKolleg\Api\Session;
     </footer>
     <script src="/dist/app.js"></script>
     <script src="/assets/math.js?config=TeX-AMS-MML_HTMLorMML,"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    <script type="text/javascript" id="MathJax-script" async
+            src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
+    </script>
 
     </body>
 </html>
