7a7106bdd377e97d3063f3d43c3612bd82359614
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 19:04:35 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -173,18 +173,7 @@ use TreptowKolleg\Api\Session;
                 displayMath: [             // start/end delimiter pairs for display math
                     ['$$', '$$'],
                     ['\\[', '\\]']
-                ],
-                processEscapes: true,      // use \$ to produce a literal dollar sign
-                processEnvironments: true, // process \begin{xxx}...\end{xxx} outside math mode
-                processRefs: true,         // process \ref{...} outside of math mode
-                digits: /^(?:[0-9]+(?:\{,\}[0-9]{3})*(?:\.[0-9]*)?|\.[0-9]+)/,
-                // pattern for recognizing numbers
-                tags: 'none',              // or 'ams' or 'all'
-                tagSide: 'right',          // side for \tag macros
-                tagIndent: '0.8em',        // amount to indent tags
-                useLabelIds: true,         // use label name rather than tag for ids
-                maxMacros: 10000,          // maximum number of macro substitutions per expression
-                maxBuffer: 5 * 1024,       // maximum size for the internal TeX string (5K)
+                ]
             },
             svg: {
                 fontCache: 'global'
