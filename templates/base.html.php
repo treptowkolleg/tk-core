9548e3c29519d04ef82bb78b2f8b3286c6dabf83
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-21 18:49:13 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -145,7 +145,6 @@ use TreptowKolleg\Api\Session;
         </div>
     </footer>
     <script src="/dist/app.js"></script>
-    <script src="/assets/math.js?config=TeX-AMS-MML_HTMLorMML,"></script>
     <script>
         MathJax = {
             tex: {
@@ -173,6 +172,9 @@ use TreptowKolleg\Api\Session;
                         '' : String(document.location).replace(/#.*$/, '')),
         }
         }
+    </script>
+    <script type="text/javascript" id="MathJax-script" async
+            src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
     </script>
 
     </body>
