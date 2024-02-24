eb9e98b817df8ca63597ded41e0c6fa4b8902bf5
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-24 09:28:14 +0100

    update

--- templates/vp.html.php
+++ templates/vp.html.php
@@ -8,6 +8,20 @@
     </div>
 </div>
 
+<div class="l-docs__meta">
+    <div class="l-docs__sticky-container">
+        <aside class="p-table-of-contents">
+            <div class="p-table-of-contents__section p-strip is-shallow">
+                <h2 class="p-table-of-contents__header">Auf dieser Seite</h2>
+                <nav class="p-table-of-contents__nav" aria-label="Table of contents">
+                    <ul class="p-table-of-contents__list">
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+    </div>
+</div>
+
 <main>
 
 
