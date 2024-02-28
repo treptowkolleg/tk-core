7d5bd6124a2378087d7a9291ba5b2f3208b44f4f
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-28 18:19:01 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -36,6 +36,7 @@ use TreptowKolleg\Api\Session;
             details{
                 background-color: white;
                 border: 1px solid dimgrey;
+                padding: 5px 5px;
             }
         </style>
         <script src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js"></script>
