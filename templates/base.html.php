91535750bff5156e089ef5a98166e45da2550ffa
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-02-28 18:59:55 +0100

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -41,7 +41,8 @@ use TreptowKolleg\Api\Session;
             details > summary{
                 cursor: pointer;
                 margin-bottom: 0!important;
-                padding-bottom: 0!important;
+                padding-top: 2px!important;
+                padding-bottom: 2px!important;
             }
         </style>
         <script src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js"></script>
