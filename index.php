028c1404b80e7759d4acb2a7e3c32b0f2710a0cd
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <123292918+treptowkolleg@users.noreply.github.com>
2024-04-21 21:11:21 +0200

    update

--- index.php
+++ index.php
@@ -78,7 +78,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if(isset($_POST['sql'])) {
-        $response = $api->requestSQL($_POST['query']);
+        try {
+            $response = $api->requestSQL($_POST['query']);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+
         $message = $response['message'];
         $columns = array_keys($response['response'][0]);
     }
