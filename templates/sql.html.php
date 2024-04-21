fdcbe2e16bcc6769984f7b4b23fed88d97c3e4a7
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <123292918+treptowkolleg@users.noreply.github.com>
2024-04-21 21:37:41 +0200

    update

--- templates/sql.html.php
+++ templates/sql.html.php
@@ -137,7 +137,7 @@ $tableCountry = [
                 </form>
             </div>
             <div class="col">
-                <?php if (isset($message)): ?>
+                <?php if (isset($message) and $message): ?>
                 <?php $messages = explode(':',$message) ?>
                     <div class="p-notification--caution">
                         <div class="p-notification__content">
