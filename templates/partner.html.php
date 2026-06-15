8486163659ce05c8ada9fb445fd661687fd7394e
Benjamin Wagner <254272419+civicsoftDE@users.noreply.github.com>
2026-06-15 23:28:32 +0200

    update

--- templates/partner.html.php
+++ templates/partner.html.php
@@ -86,23 +86,21 @@
         </div>
         <div class="row u-vertically-center">
             <div class="col-small-2 col-medium-2 col-2 u-align--center">
-                <a href="https://www.alumnihub.de" target="_blank">
+                <a href="https://www.alumnihub.de" target="_blank" style="text-decoration: none!important; color: #0d0d0d">
                     <img src="/assets/img/android-chrome-512x512.png" alt="Treptow-Kolleg Berlin" height="92" loading="eager" style="max-height:92px;">
+                    <div class="p-text--small">Alumni<b>Hub</b></div>
                 </a>
             </div>
             <div class="col-small-2 col-medium-2 col-2 u-align--center">
-                <a href="https://www.treptow-kolleg.de" target="_blank">
+                <a href="https://www.treptow-kolleg.de" target="_blank" style="text-decoration: none!important; color: #0d0d0d">
                     <img src="/assets/img/tk.png" alt="Treptow-Kolleg Berlin" height="92" loading="eager" style="max-height:92px;">
+                    <div class="p-text--small">Treptow-Kolleg</div>
                 </a>
             </div>
             <div class="col-small-2 col-medium-2 col-2 u-align--center">
-                <a href="https://www.jetbrains.com" target="_blank">
-                    <img src="https://res.cloudinary.com/canonical/image/fetch/f_svg,q_auto,fl_sanitize,w_146/https%3A%2F%2Fassets.ubuntu.com%2Fv1%2F55f6dae7-logo-jetbrains--snapcraft-homepage.svg" alt="JetBrains" height="128" loading="eager" style="max-height:128px;">
-                </a>
-            </div>
-            <div class="col-small-2 col-medium-2 col-2 u-align--center">
-                <a href="https://www.pad-berlin.de/" target="_blank">
-                    <img src="/assets/img/pad.webp" alt="pad GmbH" height="128" loading="eager" style="max-height:128px;">
+                <a href="https://www.jetbrains.com" target="_blank" style="text-decoration: none!important; color: #0d0d0d">
+                    <img src="/assets/img/jetbrains-01.svg" alt="JetBrains" height="92" loading="eager" style="max-height:92px;">
+                    <div class="p-text--small">JetBrains</div>
                 </a>
             </div>
         </div>
