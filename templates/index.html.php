1959f3a670fef6805cc9d0803e6dcfee2fdb796c
Benjamin Wagner <254272419+civicsoftDE@users.noreply.github.com>
2026-06-15 23:43:14 +0200

    update

--- templates/index.html.php
+++ templates/index.html.php
@@ -4,7 +4,7 @@
         <div class="row--50-50-on-large">
             <div class="col">
                 <div class="p-section--shallow">
-                    <h1>Startseite</h1>
+                    <h1>Aktuelles</h1>
                 </div>
             </div>
             <div class="col">
@@ -15,7 +15,20 @@
         <div class="p-section">
             <div class="row">
                 <div class="col">
-                    <h3 class="p-text--small-caps">Hinweise und so</h3>
+                    <h3>Wichtige Links</h3>
+                    <h4 class="p-text--small-caps">Brückenkurse</h4>
+                    <p>
+                        <a href="https://www.ombplus.de/ombplus/public/index.html?org=dpg" target="_blank">Online
+                            Mathematik Brückenkurs OMB+</a>
+                    </p>
+                    <p>
+                        Du willst dich auf ein Ingenieur-, Wirtschafts-, Naturwissenschafts- oder Informatikstudium
+                        vorbereiten? Dann ist der Mathekurs OMB+ genau für dich. In dem kostenlosen Mathekurs lernst du
+                        online, wann immer es dir passt. Die Tutorinnen und Tutoren im Call-Center unterstützen dich
+                        gerne dabei. Du erreichst sie täglich - auch an Wochenenden - über Chat, Telefon oder Forum. Der
+                        Kurs hilft dir den Schulstoff Mathe soweit aufzuarbeiten, dass du den Hochschulkursen dann
+                        problemlos folgen kannst.
+                    </p>
                 </div>
             </div>
         </div>
