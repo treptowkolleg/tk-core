12e5192ea54fb6b9d6c1613905d8a02233f49e89
Benjamin Wagner <254272419+civicsoftDE@users.noreply.github.com>
2026-06-15 23:46:34 +0200

    update

--- templates/base.html.php
+++ templates/base.html.php
@@ -82,7 +82,7 @@ use TreptowKolleg\Api\Session;
                             <nav class="p-navigation__nav" aria-label="Example main">
                                 <ul class="p-navigation__items">
                                     <li class="p-navigation__item <?= str_starts_with($_GET['page'], 'docs-toc.md') ? 'is-selected' : ''?>">
-                                        <a class="p-navigation__link" href="/?page=docs-toc.md">Home</a>
+                                        <a class="p-navigation__link" href="/?page=docs-toc.md">Aktuelles</a>
                                     </li>
                                     <li class="p-navigation__item <?= !str_starts_with($_GET['page'], 'docs-toc.md') && str_starts_with($_GET['page'], 'docs-') ? 'is-selected' : ''?>">
                                         <a class="p-navigation__link" href="/?page=docs-README.md">Docs</a>
