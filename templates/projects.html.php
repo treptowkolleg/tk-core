f03083972c68dd7859d624e3432f7c2d79fbfc65
Benjamin Wagner <254272419+civicsoftDE@users.noreply.github.com>
2026-06-15 21:13:05 +0200

    update

--- templates/projects.html.php
+++ templates/projects.html.php
@@ -30,6 +30,22 @@
                 <p>Inhalt folgt</p>
             </div>
         </div>
+        <div class="row--50-50-on-large p-section">
+            <div class="col">
+                <h3 class="p-text--small-caps">PKI-Software für Bundes- und Landtagswahlen</h3>
+            </div>
+            <div class="col">
+                <p>Inhalt folgt</p>
+            </div>
+        </div>
+        <div class="row--50-50-on-large p-section">
+            <div class="col">
+                <h3 class="p-text--small-caps">Machine-Vision-basierte Konferenzsysteme</h3>
+            </div>
+            <div class="col">
+                <p>Inhalt folgt</p>
+            </div>
+        </div>
     </section>
 
 </main>
